848573d2ae3d9e9a6fb36bea196a9ac1eb2b460f
maythanhsuaung1111@gmail.com <maythanhsuaung1111@gmail.com>
2024-09-03 17:02:48 +0800

    add a few designs

--- Client/src/pages/Stocks/Stock_homepage.jsx
+++ Client/src/pages/Stocks/Stock_homepage.jsx
@@ -1,7 +1,9 @@
+import React,{useState,useEffect} from 'react'
 import StableSidebar from '@/components/StableSidebar'
 import SearchStock from '@/components/stockPage/searchStock'
 import StockCard from '@/components/stockPage/StockCard'
-import React from 'react'
+
+import http from '../../../http'
 import {
     Carousel,
     CarouselContent,
@@ -9,8 +11,16 @@ import {
     CarouselNext,
     CarouselPrevious,
   } from "@/components/ui/carousel";
+import StockTable from '@/components/stockPage/StockTable';
   
 const Stock_homepage = () => {
+  const [stocks, setStocks] = useState([])
+  const fetchStocks = async () => {
+    console.log('fetching stocks')
+    }
+  useEffect(() => {
+fetchStocks()
+  }, [])
   return (
     <div>
       <StableSidebar>
@@ -18,7 +28,7 @@ const Stock_homepage = () => {
            <SearchStock/> 
            <h1 className='text-2xl font-bold ml-10 mt-10'>Trending Stocks</h1>
            <div className='mt-10 w-[90%] m-auto'>
-           <Carousel className="">
+           <Carousel>
             <CarouselContent>
               <CarouselItem className="md:basis-1/2 lg:basis-1/4"><StockCard name={'Nvidia'} desc={'NASA::2'} amount={'119'} pl={'1.78'} percent={'1.9'} duration={'1D'}/></CarouselItem>
               <CarouselItem className="md:basis-1/2 lg:basis-1/4"><StockCard name={'Nvidia'} desc={'NASA::2'} amount={'119'} pl={'1.78'} percent={'1.9'} duration={'1D'}/></CarouselItem>
@@ -29,6 +39,13 @@ const Stock_homepage = () => {
             <CarouselPrevious />
             <CarouselNext />
           </Carousel>
+          <div className='mt-[3rem]'>
+          <h1 className='text-2xl font-bold'>All Stocks</h1>
+          <div className='mt-10'>
+          <StockTable/>
+          </div>
+           
+          </div>
            </div>
         </div>
       </StableSidebar>
