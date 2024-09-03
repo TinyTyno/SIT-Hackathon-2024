0b6b02bdc8214f1c82b4edefd69cee4bc9a2a7e7
Jun Yuan <wolftoast529@gmail.com>
2024-09-03 14:55:28 +0800

    Updated View Stock

--- Client/src/components/stockPage/searchStock.jsx
+++ Client/src/components/stockPage/searchStock.jsx
@@ -18,7 +18,7 @@ function SearchStock() {
     };
 
     return (
-        <div className="flex w-full max-w-sm items-center space-x-2" style={{marginBottom:'4rem', paddingTop:'2rem', margin:'auto'}}>
+        <div className="flex w-full max-w-sm items-center space-x-2" style={{marginBottom:'4rem', paddingTop:'2rem', marginLeft: '70px'}}>
             <form onSubmit={handleSubmit} className="flex items-center space-x-2 w-full">
                 <Input 
                     value={inputValue} 
--- Client/src/pages/stockPage/viewStock.jsx
+++ Client/src/pages/stockPage/viewStock.jsx
@@ -1,11 +1,5 @@
 import React, { useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-
-import {
-    ResizableHandle,
-    ResizablePanel,
-    ResizablePanelGroup,
-  } from "@/components/ui/resizable"
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
 import axios from 'axios';
@@ -13,7 +7,7 @@ import {
     HoverCard,
     HoverCardContent,
     HoverCardTrigger,
-  } from "@/components/ui/hover-card"
+} from "@/components/ui/hover-card"
 import SearchStock from '@/components/stockPage/searchStock'
 import StableSidebar from '@/components/StableSidebar'
 
@@ -21,7 +15,7 @@ import StableSidebar from '@/components/StableSidebar'
 function ViewStock() {
     const { symbol } = useParams();
     const navigate = useNavigate();
-    
+
     useEffect(() => {
         fetchData();
     }, []);
@@ -34,122 +28,108 @@ function ViewStock() {
 
 
     return (
-        <div class="container" style={{margin:'auto'}}>
+        <div class="container" style={{ margin: 'auto' }}>
             <StableSidebar />
             <SearchStock />
-            <ResizablePanelGroup direction="horizontal" className="w-[100vw]" style={{ width: '80vw', margin:'auto', marginLeft:'70px'}}>
-            <ResizablePanel>
-            <div className="flex m-2 flex-col items-start" style={{textalign:'left', padding:'10px'}}>
-                <span className="text-4xl font-semibold tracking-tight">Nvidia</span>
-                <span className="text-gray-500 text-sm mt-1">NASDAQ:NVDA</span>
+            <div className='flex' style={{margin: 'auto', marginLeft: '70px', border: '1px solid #D9D9D9', borderRadius:'10px', marginTop:'50px' }}>
+                <div className="flex m-2 flex-col items-start" style={{ textalign: 'left', padding: '10px' }}>
+                    <span className="text-4xl font-semibold tracking-tight">Nvidia</span>
+                    <span className="text-gray-500 text-sm mt-1">NASDAQ:NVDA</span>
+                </div>
+                <div className='grow'></div>
+                <div className="flex items-center justify-center">
+                    <div className="flex space-x-4 mx-8">
+                        <HoverCard>
+                            <HoverCardTrigger>
+                                <Button className="px-7 py-5 bg-blue-500 text-white rounded hover:bg-blue-600" onClick={() => navigate(`../../buyStock/${symbol}`)}>
+                                    Buy
+                                </Button>
+                            </HoverCardTrigger>
+                            <HoverCardContent className="text-xs">
+                                Buying a stock means betting that its price will go up.
+                                If the price rises, you profit, but if it falls, you incur a loss
+                            </HoverCardContent>
+                        </HoverCard>
+                        <HoverCard>
+                            <HoverCardTrigger>
+                                <Button className="px-7 py-5 bg-red-500 text-white rounded hover:bg-red-600" onClick={() => navigate(`../../sellStock/${symbol}`)}>
+                                    Sell
+                                </Button>
+                            </HoverCardTrigger>
+                            <HoverCardContent className="text-xs">
+                                Buying a stock means betting that its price will go down.
+                                If the price falls, you profit, but if it rises, you incur a loss
+                            </HoverCardContent>
+                        </HoverCard>
+                    </div>
+                </div>                
             </div>
-            </ResizablePanel>
-            <ResizableHandle style={{display:'none'}}/>
-            <ResizablePanel defaultSize={29} minSize={24}>
-                <div className="flex h-full items-center justify-center">
-                <div className="flex space-x-4">
+
+            <div className='flex' style={{ margin: 'auto', marginLeft: '70px' }}>
+                <div className="flex m-2 flex-col items-start" style={{ textalign: 'left', padding: '10px' }}>
                     <HoverCard>
-                        <HoverCardTrigger>
-                            <Button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onClick={() => navigate(`../../buyStock/${symbol}`)}>
-                            Buy
-                            </Button>
-                        </HoverCardTrigger>
+                        <HoverCardTrigger><span className="text-5xl font-bold">119.12 <span className='text-gray-600 text-base font-bold'>USD</span></span></HoverCardTrigger>
                         <HoverCardContent className="text-xs">
-                            Buying a stock means betting that its price will go up. 
-                            If the price rises, you profit, but if it falls, you incur a loss
+                            The current price of the stock. This price is constantly changing as the stock is bought and sold.
                         </HoverCardContent>
                     </HoverCard>
                     <HoverCard>
                         <HoverCardTrigger>
-                            <Button className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600" onClick={() => navigate(`../../sellStock/${symbol}`)}>
-                            Sell
-                            </Button>
+                            <span className="text-green-600 text-base font-semibold mt-1">+ 1.79 (1.51%) <span className='text-gray-600 font-bold'>1D</span></span>
                         </HoverCardTrigger>
                         <HoverCardContent className="text-xs">
-                            Buying a stock means betting that its price will go down. 
-                            If the price falls, you profit, but if it rises, you incur a loss
+                            The change in the stock price compared to the previous day's closing price.
                         </HoverCardContent>
                     </HoverCard>
-
-
+                </div>
+                <div className='grow'></div>
+                <div className='flex flex-row'>
+                    <div className="flex flex-col h-full items-start justify-center text-sm" style={{ margin: 'auto' }}>
+                        <HoverCard>
+                            <HoverCardTrigger>
+                                <span>High: 121.75</span>
+                            </HoverCardTrigger>
+                            <HoverCardContent className="text-xs">
+                                The highest price at which a stock has traded during the day
+                            </HoverCardContent>
+                        </HoverCard>
+                        <HoverCard>
+                            <HoverCardTrigger>
+                                <span>Low: 117.22</span>
+                            </HoverCardTrigger>
+                            <HoverCardContent className="text-xs">
+                                The lowest price at which a stock has traded during the day
+                            </HoverCardContent>
+                        </HoverCard>
+                    </div>
+                    <div className='w-20'></div>
+                    <div className="flex flex-col h-full items-start justify-center text-sm" style={{ margin: 'auto' }}>
+                        <HoverCard>
+                            <HoverCardTrigger>
+                                <span>Open: 119.53</span>
+                            </HoverCardTrigger>
+                            <HoverCardContent className="text-xs">
+                                The price at which the stock first traded when the market opened for the day.
+                            </HoverCardContent>
+                        </HoverCard>
+                        <HoverCard>
+                            <HoverCardTrigger>
+                                <span>Prev Close: 117.59</span>
+                            </HoverCardTrigger>
+                            <HoverCardContent className="text-xs">
+                                The price at which the stock last traded when the market closed the previous day.
+                            </HoverCardContent>
+                        </HoverCard>
                     </div>
                 </div>
-            </ResizablePanel>
-            </ResizablePanelGroup>
-            <ResizablePanelGroup direction="horizontal" className="border w-[100vw]" style={{ width: '80vw', margin:'auto', marginLeft:'70px'}}>
-            <ResizablePanel defaultSize={73}>
-            <div className="flex m-2 flex-col items-start" style={{textalign:'left', padding:'10px'}}>
-                <HoverCard>
-                    <HoverCardTrigger><span className="text-5xl font-bold">119.12 <span className='text-gray-600 text-base font-bold'>USD</span></span></HoverCardTrigger>
-                    <HoverCardContent className="text-xs">
-                        The current price of the stock. This price is constantly changing as the stock is bought and sold.
-                    </HoverCardContent>
-                </HoverCard>
-                <HoverCard>
-                    <HoverCardTrigger>
-                        <span className="text-green-600 text-base font-semibold mt-1">+ 1.79 (1.51%) <span className='text-gray-600 font-bold'>1D</span></span>
-                    </HoverCardTrigger>
-                    <HoverCardContent className="text-xs">
-                        The change in the stock price compared to the previous day's closing price.
-                    </HoverCardContent>
-                </HoverCard>
             </div>
-            </ResizablePanel>
-            <ResizableHandle style={{display:'none'}}/>
-            <ResizablePanel>
-            <div className="flex flex-col h-full items-start justify-center text-sm" style={{margin:'auto'}}>
-                <HoverCard>
-                    <HoverCardTrigger>
-                    <span>High: 121.75</span>
-                    </HoverCardTrigger>
-                    <HoverCardContent className="text-xs">
-                        The highest price at which a stock has traded during the day
-                    </HoverCardContent>
-                </HoverCard>
-                <HoverCard>
-                    <HoverCardTrigger>
-                    <span>Low: 117.22</span>
-                    </HoverCardTrigger>
-                    <HoverCardContent className="text-xs">
-                        The lowest price at which a stock has traded during the day
-                    </HoverCardContent>
-                </HoverCard>
-            </div>
-            </ResizablePanel>
-            <ResizableHandle style={{display:'none'}} />
-            <ResizablePanel >
-            <div className="flex flex-col h-full items-start justify-center text-sm" style={{margin:'auto'}}>
-                <HoverCard>
-                    <HoverCardTrigger>
-                    <span>Open: 119.53</span>
-                    </HoverCardTrigger>
-                    <HoverCardContent className="text-xs">
-                        The price at which the stock first traded when the market opened for the day.
-                    </HoverCardContent>
-                </HoverCard>
-                <HoverCard>
-                    <HoverCardTrigger>
-                    <span>Prev Close: 117.59</span>
-                    </HoverCardTrigger>
-                    <HoverCardContent className="text-xs">
-                        The price at which the stock last traded when the market closed the previous day.
-                    </HoverCardContent>
-                </HoverCard>
-            </div>
-            </ResizablePanel>
-            </ResizablePanelGroup>
-            <ResizablePanelGroup direction="horizontal" className="border w-[100vw]" style={{ width: '80vw', margin:'auto', marginLeft:'70px'}}>
-            <ResizablePanel>
-                {/* // Put your charts here */}
-            </ResizablePanel>
-            </ResizablePanelGroup>
         </div>
     )
 }
 
 export default ViewStock
 
-  
+
 
 
 
