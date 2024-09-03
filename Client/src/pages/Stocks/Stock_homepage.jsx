94d98cc8a2bf5ab0e4769f753df6d604980a218a
Miguel <animatorpizzaguy74@gmail.com>
2024-09-03 22:59:09 +0800

    Updated the component SearchStock in Stock Home Page

--- Client/src/pages/Stocks/Stock_homepage.jsx
+++ Client/src/pages/Stocks/Stock_homepage.jsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import StableSidebar from '@/components/StableSidebar'
-import SearchStock from '@/components/stockPage/searchStock'
+import SearchStockInput from '@/components/stockPage/searchStockInput'
 import StockCard from '@/components/stockPage/StockCard'
 import axios from 'axios'
 import {
@@ -29,7 +29,7 @@ fetchData()
     <div>
       <StableSidebar>
         <div className="w-[90%] m-auto">
-           <SearchStock/> 
+           <SearchStockInput/> 
            <h1 className='text-2xl font-bold ml-10 mt-10'>Trending Stocks</h1>
            <div className='mt-10 w-[90%] m-auto'>
            <Carousel>
